3d3e1cd8dc53d88c7e961ba96a31a1d451d89282
spadaritucks <thiago.spadari02@gmail.com>
2024-11-03 20:08:22 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +6,30 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'fitclub-teste';
+
+  constructor(private renderer: Renderer2) {}
+
+  // Escuta o evento de rolagem da janela
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+    // Define o ponto em que a animação deve ocorrer
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+
+      // Adiciona a classe 'visible' quando a seção está visível na tela
+      if (sectionTop < windowHeight - 100) {
+        this.renderer.addClass(section, 'visible');
+      } else {
+        this.renderer.removeClass(section, 'visible');
+      }
+    });
+  }
 }
